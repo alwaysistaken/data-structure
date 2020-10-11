52fd05a875c75c7a5e99010fa51d76a2c029f9f7
me <pli739822@gmail.com>
2020-10-11 09:46:48 +0800

    队列

---
+++ queue.js
@@ -0,0 +1,31 @@
+//队列的主要方法 ： 入队、出队、根据index查询队列元素、获取队列长度，根据数组直接创建队列,打印队列
+
+class Queue {
+
+    queue = [];
+
+    //根据数组初始化队列
+    initQueue = function(Array) {
+
+        Array.forEach(element => {
+
+        });
+    }
+
+    isEmpty = function() {
+
+    }
+
+    getLength = function() {
+
+    }
+
+    insert = function() {
+
+    }
+
+    del = function() {
+
+    }
+
+}
